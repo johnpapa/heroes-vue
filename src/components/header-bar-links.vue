dcd0a9520488d15342efd6234cad27deefeca845
John Papa <john@johnpapa.net>
2018-11-07 17:05:37 +0000

    refactored header into sub components

---
+++ src/components/header-bar-links.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a href="https://github.com/johnpapa/heroes-vue" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a href="https://twitter.com/john_papa" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBarLinks',
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/header-bar.vue
+++ src/components/header-bar.vue
@@ -1,42 +1,19 @@
 <template>
   <header class="">
     <nav class="navbar has-background-dark is-dark" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">
-          <i class="fab js-logo fa-vuejs fa-2x" aria-hidden="true" />
-        </a>
-        <router-link to="/" class="navbar-item nav-home">
-          <span class="tour">TOUR</span>
-          <span class="of">OF</span>
-          <span class="heroes">HEROES</span>
-        </router-link>
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-        </a>
-      </div>
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a href="https://github.com/johnpapa/heroes-vue" target="_blank" rel="noopener noreferrer">
-                <i class="fab fa-github fa-2x" aria-hidden="true"></i>
-              </a>
-              <a href="https://twitter.com/john_papa" target="_blank" rel="noopener noreferrer">
-                <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <HeaderBarBrand></HeaderBarBrand>
+      <HeaderBarLinks></HeaderBarLinks>
     </nav>
   </header>
 </template>
 
 <script>
+import HeaderBarBrand from '@/components/header-bar-brand.vue';
+import HeaderBarLinks from '@/components/header-bar-links.vue';
+
 export default {
   name: 'HeaderBar',
+  components: { HeaderBarBrand, HeaderBarLinks },
 };
 </script>
 
