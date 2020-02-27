9827e78c9c95a7d847fd870dea24439a6c1c2b33
John Papa <papa@Johns-MacBook-Pro-16.local>
2020-02-27 14:32:51 -0500

    hmmm

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 9626;
-const www = process.env.WWW || './public';
+const www = process.env.WWW || './dist';
 
 const captains = console;
 
