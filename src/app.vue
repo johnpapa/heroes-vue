6c12cdc31d4ae35c42dc937c256c981174ca0290
John Papa <john@johnpapa.net>
2020-12-02 17:11:08 -0500

    app component fixes

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,3 @@
-<template>
-  <div id="app">
-    <HeaderBar />
-    <div class="section columns">
-      <NavBar />
-      <main class="column">
-        <router-view />
-      </main>
-    </div>
-  </div>
-</template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HeaderBar from '@/components/header-bar.vue';
@@ -24,13 +12,18 @@ export default defineComponent({
 });
 </script>
 
+<template>
+  <div id="app">
+    <HeaderBar />
+    <div class="section columns">
+      <NavBar />
+      <main class="column">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import 'styles.scss';
 </style>
